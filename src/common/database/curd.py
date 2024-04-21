7b1764e258f9b5c91c9ae60bc1e6060e0d2d8465
bemake-pycharm <271374667@qq.com>
2024-04-21 19:25:42 +0800

    Feature: 实现数据库增删改查以及重构模型为mapped解决类型不正确的问题

---
+++ src/common/database/curd.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+from typing import Type
+
+from sqlalchemy.orm import Query
+from sqlalchemy.orm import Session
+
+from src.common.database.model import MMD, Tag
+from src.common.database.session import session
+
+
+class Curd:
+    def __init__(self):
+        self._session: Session = session
+
+    def add(self,
+            mmd_id: int,
+            post_time: datetime,
+            author: str,
+            pic_size: str,
+            pic_url: str,
+            source: str,
+            rating: str,
+            score: int,
+            tags: str,
+            url: str,
+            create_time: datetime,
+            update_time: datetime,
+            status: int,
+            download_status: int,
+            # 下面是tag的信息
+            tag_en_name: str,
+            tag_cn_name: str,
+            tag_create_time: datetime
+            ):
+        tag = Tag(tag_en_name=tag_en_name,
+                  tag_cn_name=tag_cn_name,
+                  create_time=tag_create_time)
+
+        mmd = MMD(mmd_id=mmd_id,
+                  post_time=post_time,
+                  author=author,
+                  pic_size=pic_size,
+                  pic_url=pic_url,
+                  source=source,
+                  rating=rating,
+                  score=score,
+                  tags=tags,
+                  url=url,
+                  create_time=create_time,
+                  update_time=update_time,
+                  status=status,
+                  download_status=download_status,
+                  tag=tag
+                  )
+        self._session.add(mmd)
+        self._session.commit()
+
+    def remove_by_mmd_id(self, mmd_id: int):
+        mmd = self._session.query(MMD).filter(MMD.mmd_id == mmd_id).first()
+        self._session.delete(mmd)
+        self._session.commit()
+
+    def update_by_mmd_id(self, mmd_id: int, **kwargs):
+        mmd = self._session.query(MMD).filter(MMD.mmd_id == mmd_id).first()
+        for key, value in kwargs.items():
+            setattr(mmd, key, value)
+        self._session.commit()
+
+    def get_mmd_query(self) -> Query[Type[MMD]]:
+        return self._session.query(MMD)
+
+    def get_tag_query(self) -> Query[Type[Tag]]:
+        return self._session.query(Tag)
+
+
+if __name__ == '__main__':
+    curd = Curd()
+    curd.add(1, datetime.now(), 'test', 'test', 'test', 'test', 'test', 1, 'test', 'test', datetime.now(),
+             datetime.now(), 0, 0, 'test', 'test', datetime.now())
+    print('add success')
+    curd.get_mmd_query()
--- src/common/database/model.py
+++ src/common/database/model.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
@@ -8,47 +8,35 @@ class Base(DeclarativeBase):
 
 class MMD(Base):
     __tablename__ = 'mmd'
-    id = Column(Integer, primary_key=True)
-    # 每一个MMD都有一个唯一的mmd_id,网站上的id
-    mmd_id = Column(Integer)
-    # 模型上传时间
-    post_time = Column(DateTime)
-    # 模型作者
-    author = Column(String(32))
-    # 模型详情页面预览图的尺寸
-    pic_size = Column(String(16))
-    # 模型详情页面预览图的url
-    pic_url = Column(String(256))
-    # 模型的下载地址
-    source = Column(String(256))
-    # 模型的分级
-    rating = Column(String(16))
-    # 模型的评分
-    score = Column(Integer)
-    # 模型的标签,用#分割,比如#tag1#tag2
-    tags = Column(String(512))
-    # 与tag表的关联
-    tag_id = Column(Integer, ForeignKey('tag.id'))
-    tag = relationship('Tag', back_populates='mmds')
-    # 模型详情页面的url
-    url = Column(String(256))
-    # 这一条记录的创建时间
-    create_time = Column(DateTime)
-    # 这一条记录的更新时间
-    update_time = Column(DateTime)
-    # 这一条记录的状态,0表示还未看过,1表示已经看过
-    status = Column(Integer)
-    # 这一条记录的下载状态,0表示无法下载(比如链接失效),1表示可以下载,2代表未知网站
-    download_status = Column(Integer)
+    id: Mapped[Integer] = mapped_column(Integer, primary_key=True)  # 每一个MMD都有一个唯一的id
+    mmd_id: Mapped[Integer] = mapped_column(Integer)  # 网站上的id
+    post_time: Mapped[DateTime] = mapped_column(DateTime)  # 模型上传时间
+    author: Mapped[String] = mapped_column(String(32))  # 模型作者
+    pic_size: Mapped[String] = mapped_column(String(16))  # 模型详情页面预览图的尺寸
+    pic_url: Mapped[String] = mapped_column(String(256))  # 模型详情页面预览图的url
+    source: Mapped[String] = mapped_column(String(256))  # 模型的下载地址
+    rating: Mapped[String] = mapped_column(String(16))  # 模型的分级
+    score: Mapped[Integer] = mapped_column(Integer)  # 模型的评分
+    tags: Mapped[String] = mapped_column(String(512))  # 模型的标签,用#分割,比如#tag1#tag2
+    tag_id: Mapped[Integer] = mapped_column(Integer, ForeignKey('tag.id'))  # 与tag表的关联
+    tag: Mapped[relationship] = relationship('Tag', back_populates='mmds')
+    url: Mapped[String] = mapped_column(String(256))  # 模型详情页面的url
+    create_time: Mapped[DateTime] = mapped_column(DateTime)  # 这一条记录的创建时间
+    update_time: Mapped[DateTime] = mapped_column(DateTime)  # 这一条记录的更新时间
+    status: Mapped[Integer] = mapped_column(Integer)  # 这一条记录的状态,0表示还未看过,1表示已经看过
+    download_status: Mapped[Integer] = mapped_column(Integer)  # 这一条记录的下载状态,0表示无法下载(比如链接失效,1表示可以下载,2代表未知网站
+
+    def __repr__(self) -> str:
+        return f'<MMD(mmd_id={self.mmd_id}, post_time={self.post_time}, author={self.author}, pic_size={self.pic_size}, pic_url={self.pic_url}, source={self.source}, rating={self.rating}, score={self.score}, tags={self.tags}, url={self.url}, create_time={self.create_time}, update_time={self.update_time}, status={self.status}, download_status={self.download_status})>'
 
 
 class Tag(Base):
     __tablename__ = 'tag'
-    id = Column(Integer, primary_key=True)
-    # tag的英文名字
-    tag_en_name = Column(Integer)
-    # tag的中文名字(调用翻译API翻译过来的)
-    tag_cn_name = Column(String(32))
-    # tag的创建时间
-    create_time = Column(DateTime)
-    mmds = relationship('MMD', back_populates='tag')
+    id: Mapped[Integer] = mapped_column(Integer, primary_key=True)  # tag的id
+    tag_en_name: Mapped[Integer] = mapped_column(Integer)  # tag的英文名字
+    tag_cn_name: Mapped[String] = mapped_column(String(32))  # tag的中文名字(调用翻译API翻译过来的
+    create_time: Mapped[DateTime] = mapped_column(DateTime)  # tag的创建时间
+    mmds: Mapped[relationship] = relationship('MMD', back_populates='tag')
+
+    def __repr__(self) -> str:
+        return f'<Tag(tag_en_name={self.tag_en_name}, tag_cn_name={self.tag_cn_name}, create_time={self.create_time})>'
