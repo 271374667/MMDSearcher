911db5d1612b490786b1d7f5ca75a805575c1957
potato <2444265157@qq.com>
2024-04-25 22:58:31 +0800

    Feature: 80%完成HomeMain组件静态页面

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-view">
+    <HomeMain></HomeMain>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HomeMain from '@/components/Home/HomeMain.vue';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    HomeMain,
+  },
+};
 </script>
+
+<style lang="less" scoped></style>
