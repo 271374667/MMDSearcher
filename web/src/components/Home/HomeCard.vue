ba6b5af3e6b038b835ca758ee45d833b9ba971a1
potato <2444265157@qq.com>
2024-05-18 16:57:41 +0800

    Style: 基本大修全部组件的样式(应该实现了响应式)

--- web/src/components/Home/HomeCard.vue
+++ web/src/components/Home/HomeCard.vue
@@ -1,49 +1,42 @@
 <template>
-	<el-badge value="已读" class="item" type="primary">
-		<!-- <div class="confirm">已读</div> -->
-		<el-card :body-style="{ padding: '0px' }">
-			<img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-			<div style="padding: 14px;">
-				<span>好吃的汉堡</span>
-				<div style="padding-top: 10px;">
-					<i>作者:</i>
-					<i>级别:</i>
-					<i>评分:</i>
-					<!-- <el-button type="text" class="button">操作按钮</el-button> -->
+	<div class="card">
+		<el-badge value="已读" type="primary">
+			<el-card>
+				<img
+					src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+					class="image" />
+				<div style="padding: 14px">
+					<span>好吃的汉堡</span>
+					<div>
+						<div class="confirm">
+							<span>作者:</span>
+							<span>级别:</span>
+							<span>评分:</span>
+						</div>
+					</div>
 				</div>
-			</div>
-		</el-card>
-	</el-badge>
+			</el-card>
+		</el-badge>
+	</div>
 </template>
 
-<script>
-</script>
+<script></script>
 
-<style lang="less" scoped>
-	.confirm{
-		width: 70px;
-		height: 70px;
-		border-radius: 50%;
-		background-color: aqua;
-		text-align: center;
-		line-height: 70px;
-		font-size: 18px;
-		transform: translate(263px, 50%);
-	}
-	.image{
-		width: 100%;
-		display: block;
-	}
-	.button{
+<style lang="less">
+.card {
+	margin: 1.5rem;
+
+	// 这个.el-card__body样式不准删！！！！！！！！
+	.el-card__body {
 		padding: 0;
-		float: right;
 	}
-	span{
-		font-size: 22px;
-		display: block;
+
+	.confirm {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		font-size: 1rem;
+		color: #999;
 	}
-	i{
-		font-size: 16px;
-		display: block;
-	}
+}
 </style>
